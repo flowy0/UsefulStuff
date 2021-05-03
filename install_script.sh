93a687ce77fe8dc650ad86526bc6e5e71e9436e4
Gayle <9532712+flowy0@users.noreply.github.com>
2021-05-03 17:57:46 +0800

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -4,6 +4,9 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# install xcode developer tools
+xcode-select --install
+
 # zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
