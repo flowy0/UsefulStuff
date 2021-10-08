0ed458b66fec98e2c3f537517bd0fa296702f0bf
Gayle <9532712+flowy0@users.noreply.github.com>
2021-10-08 12:19:21 +0800

    Create install_fonts.sh

---
+++ install_fonts.sh
@@ -0,0 +1,6 @@
+# add more fonts
+# previews at https://www.nerdfonts.com/font-downloads
+
+brew install --cask font-aurulent-sans-mono-nerd-font
+brew install --cask font-hurmit-nerd-font
+brew install --cask font-fantasque-sans-mono-nerd-font
