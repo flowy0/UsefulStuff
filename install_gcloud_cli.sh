bc7b4167c55b6b145e21c144b6ec5cf773132ded
Gayle <9532712+flowy0@users.noreply.github.com>
2022-01-19 18:01:45 +0800

    update gcloud_cli script to add in file download

--- install_gcloud_cli.sh
+++ install_gcloud_cli.sh
@@ -1,1 +1,3 @@
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-368.0.0-darwin-x86_64.tar.gz
+tar -xvf google-cloud-sdk-368.0.0-darwin-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --install-python TRUE --path-update TRUE --quiet
