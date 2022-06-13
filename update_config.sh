3a290051bcb92fe031058ff0b0f145091e68dde6
Gayle <9532712+flowy0@users.noreply.github.com>
2022-06-13 17:03:01 +0800

    Update update_config.sh

--- update_config.sh
+++ update_config.sh
@@ -1,4 +1,4 @@
 # download my gitignore file
-curl "https://raw.githubusercontent.com/flowy0/dotfiles/main/.gitignore_global" --output .gitignore_global
+curl "https://raw.githubusercontent.com/flowy0/dotfiles/main/.gitignore_global" --output $HOME/.gitignore_global
 # set in git config
 git config --global core.excludesfile ~/.gitignore_global
